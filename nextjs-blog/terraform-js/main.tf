86b117af8c89877cb07ac29a90cc33c1a7424a10
Arion <arionnagappen@gmail.com>
2025-03-23 18:45:03 +0200

    cloudfront config & deployment

--- nextjs-blog/terraform-js/main.tf
+++ nextjs-blog/terraform-js/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
   region =  "af-south-1"
 }
 
+/* S3 BUCKET CONFIGURATIONS */
+
 # S3 Bucket to store static files for the website
 resource "aws_s3_bucket" "nextjs_bucket" {
   bucket = "nextjs-portfolio-bucket-an"
@@ -43,16 +45,73 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
-  policy = jsonencode(({
-    version = "2012-10-17"
+  policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
         Sid = "PublicReadGetObject"
         Effect = "Allow"
         Principal = "*"
         Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.nextjs_bucket.id.arn}/*"
+        Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
       }
     ]
-  }))
+  })
+}
+
+/* CLOUDFRONT CONFIGURATIONS */
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio site"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id = "S3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled = true # Activate CloudFront Distribution to serve content
+  is_ipv6_enabled = true # Enables IPv6 support
+  comment = "Next.js portfolio site"
+  default_root_object = "index.html" # File to be served
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"] # HTTP methods allowed for caching behaviour
+    cached_methods = ["GET", "HEAD"] # Methods to be cached
+    target_origin_id = "S3-nextjs-portfolio-bucket" # Links cache behaviour to the origin
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+
+    }
+
+    viewer_protocol_policy = "redirect-to-https" # Ensures secure connection between CDN & Client
+    min_ttl = 0 # Min. amount of time an object is cached
+    default_ttl = 3600 # Default amount of time an object is cached
+    max_ttl = 86400 # Max. amount of time an object is cached
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none" # All geographic locations are allowed
+    }
+  }
+
+  viewer_certificate {
+    # Configure SSL and TSL settings
+    cloudfront_default_certificate = true
+  }
+
 }
--- nextjs-blog/terraform-js/state.tf
+++ nextjs-blog/terraform-js/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "an-my-terraform-state" # The S3 bucket where the state file will be stored.
+    bucket = "an-my-tf-website-state" # The S3 bucket where the state file will be stored.
     key = "global/s3/terraform.tfstate" # The path within the S3 bucket to store the state file.
     region = "af-south-1"  # The AWS region where the S3 bucket is located (South Africa).
     use_lockfile = true # Enables state locking to prevent simultaneous changes
